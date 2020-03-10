180c6fcd3896fc5a16f9a413ed59b3c8167d2746
syedsameer99 <59228461+syedsameer99@users.noreply.github.com>
2020-03-10 01:10:11 -0700

    implimeted firebase utils,including ability to store suthenticated user into our firebase database

--- src/App.js
+++ src/App.js
@@ -1,63 +1,67 @@
-import React from 'react';
-import './App.css';
-import HomePage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop.component';
-import {Switch,Route} from 'react-router-dom';
-import Header from './components/header/header.component';
-import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import {auth} from './firebase/firebase.utils';
+import React from "react";
+import "./App.css";
+import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
+import { Switch, Route } from "react-router-dom";
+import Header from "./components/header/header.component";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
+// const Hatspage=()=>(
+//   <div>
+//     <h1>Hats Page</h1>
+//   </div>
+// );
 
-  // const Hatspage=()=>(
-  //   <div>
-  //     <h1>Hats Page</h1>
-  //   </div>
-  // );
+class App extends React.Component {
+  constructor() {
+    super();
 
-  class App extends React.Component {
-    constructor() {
-      super();
-    
-      this.state = {
-         currentUser:null
+    this.state = {
+      currentUser: null
+    };
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+        });
+        
       }
-    }
+      else{
+        this.setState({currentUser:userAuth});
+      }
+    });
+  }
 
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
 
-    unsubscribeFromAuth=null;
-
-
-    componentDidMount(){
-      this.unsubscribeFromAuth=auth.onAuthStateChanged(user=>{
-        this.setState({currentUser:user});
-
-        console.log(user);
-      })
-    }
-    
-    componentWillUnmount(){
-      this.unsubscribeFromAuth();
-    }
-    
-
-    render(){
+  render() {
     return (
       <div>
-        <Header currentUser={this.state.currentUser}/>
+        <Header currentUser={this.state.currentUser} />
 
         <Switch>
-          <Route exact path='/' component={HomePage}/>
-          <Route exact path='/shop' component={ShopPage}/>
-          <Route exact path='/signin' component={SignInAndSignUpPage}/>
-
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/shop" component={ShopPage} />
+          <Route exact path="/signin" component={SignInAndSignUpPage} />
         </Switch>
-
       </div>
-    
-      
-      );
-    }
-
+    );
   }
+}
 
 export default App;
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,27 +1,50 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
 
 const config = {
+  apiKey: "AIzaSyBSEM-JNIo-BqTS5stUdM_xRvtTX5KjB9Q",
+  authDomain: "crwn-db-4a4c9.firebaseapp.com",
+  databaseURL: "https://crwn-db-4a4c9.firebaseio.com",
+  projectId: "crwn-db-4a4c9",
+  storageBucket: "crwn-db-4a4c9.appspot.com",
+  messagingSenderId: "621070002219",
+  appId: "1:621070002219:web:710d803672e9539f87a26e",
+  measurementId: "G-977KPMY7VF"
+};
 
-        apiKey: "AIzaSyBSEM-JNIo-BqTS5stUdM_xRvtTX5KjB9Q",
-        authDomain: "crwn-db-4a4c9.firebaseapp.com",
-        databaseURL: "https://crwn-db-4a4c9.firebaseio.com",
-        projectId: "crwn-db-4a4c9",
-        storageBucket: "crwn-db-4a4c9.appspot.com",
-        messagingSenderId: "621070002219",
-        appId: "1:621070002219:web:710d803672e9539f87a26e",
-        measurementId: "G-977KPMY7VF"
-      
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`user/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log("error creating user", error.message);
+    }
+  }
+  return userRef;
 };
 
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
-export const firestore =firebase.firestore();
+export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({prompt: 'select_account'});
+provider.setCustomParameters({ prompt: "select_account" });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
